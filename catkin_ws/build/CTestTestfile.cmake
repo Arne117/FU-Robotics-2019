d47cbf8197c7c36f1c0207e5bf5507f1e769b1af
SidneySteimel <sixd97@gmail.com>
2019-08-12 23:12:55 +0200

    built and added a catkin workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sidney/workspace/robotic_2019/catkin_ws/src
+# Build directory: /home/sidney/workspace/robotic_2019/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
