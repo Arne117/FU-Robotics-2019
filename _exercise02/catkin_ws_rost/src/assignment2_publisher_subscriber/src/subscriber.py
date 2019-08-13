f6e2caf377aa1f23fe41b8a09f8248f3d151f000
Arne <arne.rolf92@gmail.com>
2019-08-14 00:38:56 +0200

    Exercise 2 - wip

---
+++ _exercise02/catkin_ws_rost/src/assignment2_publisher_subscriber/src/subscriber.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# RAN SCAN SUBSCRIBER
+
+print("running subscriber")
+
+import rospy
+# from rplidar_decoder import PointCloudConverter
+
+from autominy_msgs import Speed
+# import autominy_msgs
+# from autominy_msgs import *
+
+print(Speed)
+print(autominy_msgs)
+
+def callback(raw_msg):
+    print(raw_msg)
+
+# Initialize node
+# rospy.init_node("pointcloud_node")
+
+# Initialize subscriber
+
+# rospy.Subscriber("/sensors/speed", Speed, callback)
+
+# rospy.spin()
+
+
+
+######################
+# ROS Example
+######################
+# import rospy
+# from std_msgs.msg import String
+#
+# def callback(data):
+#     rospy.loginfo("I heard %s",data.data)
+#
+# def listener():
+#     rospy.init_node('node_name')
+#     rospy.Subscriber("chatter", String, callback)
+#     # spin() simply keeps python from exiting until this node is stopped
+#     rospy.spin()
